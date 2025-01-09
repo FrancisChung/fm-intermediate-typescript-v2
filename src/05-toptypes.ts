11367f04b0a800500e27a1eca1c50a294385c74c
Francis Chung <francis.c.chung@gmail.com>
2025-01-09 15:29:02 +1100

    Top Type - unknown

--- src/05-toptypes.ts
+++ src/05-toptypes.ts
@@ -8,3 +8,20 @@ flexible = setTimeout
 console.log(window, promise, setTimeout, 'foo')
 
 // unknown
+
+let flexible2: any = 4
+flexible2 = window.document
+flexible2 = setTimeout
+
+let myUnknown : unknown = 14
+
+if (typeof myUnknown === 'string') {
+    myUnknown
+} else if (typeof myUnknown === 'number') {
+    myUnknown
+} else {
+    myUnknown
+
+
+}
+
