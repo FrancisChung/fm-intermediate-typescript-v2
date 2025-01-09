3c1f3d697da8eb7f16433527ae584462f5461d6a
Francis Chung <francis.c.chung@gmail.com>
2025-01-09 17:02:29 +1100

    Void Types

--- src/07-unittypes.ts
+++ src/07-unittypes.ts
@@ -5,3 +5,13 @@ let myUndefined : undefined = undefined
 
 myNull = undefined
 myUndefined = null
+
+// Void Types
+
+let myVoid : void = (function() {})()
+
+myVoid = undefined
+myVoid = null
+
+myUndefined = myVoid
+myNull = myVoid
