da4b7c74be9fe792da43c3b4750f4be52ccc8a27
Francis Chung <francis.c.chung@gmail.com>
2025-01-13 14:39:26 +1100

    Namespace imports

--- src/12-modules-cjs-interop/index.ts
+++ src/12-modules-cjs-interop/index.ts
@@ -1,5 +1,18 @@
-import {Raspberry} from "./berries/raspberry"
+// ES Modules named imports & exports
+import { Blueberry, Raspberry } from './berries'
+import Kiwi from './kiwi' // default import
+export function makeFruitSalad() {} // named export
+export default class FruitBasket {} // default export
+export { lemon, lime } from './citrus' // re-export
+export * as berries from './berries' //
+
 
 let x: Raspberry = {color: "red"}
+
+// Namespace imports
+import * as allBerries from './berries'
+allBerries.Strawberry
+allBerries.Blueberry
+allBerries.Raspberry
 
 export * as berries from "./berries'
