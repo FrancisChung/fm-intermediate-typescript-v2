79eb620510d49c7ae216325c204bd0429c2c3710
Francis Chung <francis.c.chung@gmail.com>
2025-01-13 15:50:25 +1100

    Initial Version

---
+++ src/13-importing-nonTSthings.ts
@@ -0,0 +1,11 @@
+// Import non TS Things
+
+import img from './ts-logo.png'
+img
+
+// //Add to global.d.ts
+// declare module '*.png' {
+//     const imgURL : string
+//     export default imgurl
+// }
+// export default
