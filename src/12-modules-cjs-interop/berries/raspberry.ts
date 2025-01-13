7c69da354cfa51b965e6589315e17de43c5e86a8
Francis Chung <francis.c.chung@gmail.com>
2025-01-13 14:38:21 +1100

    Initial Version

---
+++ src/12-modules-cjs-interop/berries/raspberry.ts
@@ -0,0 +1,8 @@
+export class Raspberry {
+    constructor(public color: 'red' | 'black') {}
+}
+
+export function pickRaspberries(time: number): Raspberry[] {
+    console.log('picking raspberries')
+    return []
+}
