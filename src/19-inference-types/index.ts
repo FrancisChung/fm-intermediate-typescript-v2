9e25cb2957a0958e072ef3dd83d1e2e04d5a8965
Francis Chung <francis.c.chung@gmail.com>
2025-02-08 13:36:50 +1100

    More infer examples

--- src/19-inference-types/index.ts
+++ src/19-inference-types/index.ts
@@ -1,7 +1,23 @@
 import {createOrder}  from "./fruit-market"
 
-type GetFirstArg<T> = any
+// type GetFirstArg<T> = any
 
 const prefs: GetFirstArg<typeof createOrder> = {}
 
 createOrder(prefs)
+
+//
+
+
+// Infer only works on T below (1st part of the conditional) and not the P  (2nd part)
+type UnwrapPromise<P> = P extends PromiseLike<infer T> ? T : P
+
+type inftest1 = UnwrapPromise<Promise<string>>
+type inftest2 = UnwrapPromise<Promise<[string[], number[]]>>
+type inftest3 = UnwrapPromise<Promise<number>>
+
+type OneArgFn<A = any> = (firstArg: A, ... _args: any[]) => void
+
+type GetFirstArg<T extends OneArgFn>
+    = T extends OneArgFn ? string[] : never;
+
