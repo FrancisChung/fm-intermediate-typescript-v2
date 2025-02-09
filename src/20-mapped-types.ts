24ce88419fa83749e63f34383fbfc9bb90c0ff61
Francis Chung <francis.c.chung@gmail.com>
2025-02-09 18:35:44 +1100

    Record & Pick examples

--- src/20-mapped-types.ts
+++ src/20-mapped-types.ts
@@ -13,15 +13,45 @@ fruitCatalog.apple
 
 // First Mapped Type
 
-type MyRecord = {[FruitKey in 'apple' | 'cherry']: Fruit}
+//type MyRecord = {[FruitKey in 'apple' | 'cherry']: Fruit}
 
 function printFruitCatalog(fruitCatalog: MyRecord) {
     fruitCatalog.cherry
     fruitCatalog.apple
-    fruitCatalog.pineapple
+    //fruitCatalog.pineapple // can only be apple or cherry
 }
 
 // A Record is a dictionary with specific keys (e.g. Cherry + Apple above)
 
+// Record Examples
+
+type AnyPossibleKey = keyof any
+type MyRecord<K extends keyof any, V> = {[Key in K]: V}
+
+type Record<K extends keyof any, T>= {[P in K]: T}
+
+const myFruit : Record<"first" | "second", Fruit>
+//myFruit.first
+//myFruit.second
+
+//const y: Window["document"]
+
+type PartOfWindow = {
+    [Key in "document" | "navigator" | "setTimeout"] : Window[Key]
+}
+
+type PickWindowProperties<Keys extends keyof Window> = {
+    [Key in Keys]: Window[Key]
+}
+
+// Pick
+
+type PartOfWindow2 = PickWindowProperties<"document" | "navigator" | "setTimeout">
+
+type PickProperties<ValueType, Keys extends keyof ValueType> = {
+    [Key in Keys]: ValueType[Key]
+}
+
+type PartOfWindow3 = PickProperties<Window, "document" | "navigator" | "setTimeout">
 
 
