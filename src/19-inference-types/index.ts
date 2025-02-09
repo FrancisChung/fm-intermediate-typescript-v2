f2fa744f05d28f7d56d5bd158456a7facb0cfb18
Francis Chung <francis.c.chung@gmail.com>
2025-02-09 11:37:06 +1100

    Utility Types infer example

--- src/19-inference-types/index.ts
+++ src/19-inference-types/index.ts
@@ -44,3 +44,62 @@ const t1 = ['success', 2, 1, 4] as const
 const t2 = [4, 54, 5]
 let firstT1 : GetFirstStringIshElement<typeof t1>
 let firstT2 : GetFirstStringIshElement<typeof t2>
+
+
+//* Parameters<T>
+
+/**
+ * Obtain the parameters of a function type in a tuple
+ */
+type _Parameters
+/**
+ * The typeParam passed in, must be some subtype of a call signature,
+ * which can take any number of arguments of any types, and can
+ * have any return type
+ */
+    <T extends (...args: any) => any>
+/**
+ * As long as `T` matches a call signature, capture all of the args
+ * (as a ...rest) parameter in a new tuple typeParam `P`
+ */
+    = T extends (...args: infer P) => any
+    ? P // and then return the tuple
+    : never; // or return never, if the condition is not matched
+
+//* ConstructorParameters<T>
+/**
+ * Obtain the parameters of a constructor function type in a tuple
+ */
+type _ConstructorParameters
+/**
+ * The typeParam passed in, must be some subtype of a construct
+ * signature.
+ *
+ * The `abstract` keyword lets this also work with abstract classes,
+ * which can potentially have an `abstract` constructor
+ */
+    <T extends abstract new (...args: any) => any>
+/**
+ * As long as `T` matches a construct signature, capture all of the
+ * args (as a ...rest) parameter in a new tuple typeParam `P`
+ */
+    = T extends abstract new (...args: infer P) => any
+    ? P // and then return the tuple
+    : never; // or return never, if the condition is not matched
+
+//* ReturnType<T>
+/**
+ * Obtain the return type of a function type
+ */
+type _ReturnType
+/**
+ * The typeParam passed in must be some subtype of a call signature
+ */
+    <T extends (...args: any) => any>
+/**
+ * As long as `T` matches the call signature, capture the return type
+ * in a new typeParam `R`
+ */
+    = T extends (...args: any) => infer R
+    ? R // and then return it
+    : any; // otherwise return any
