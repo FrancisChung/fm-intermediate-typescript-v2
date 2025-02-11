a239e8e51dd32f998ff95b43133d2ae49996d71f
Francis Chung <francis.c.chung@gmail.com>
2025-02-11 17:07:34 +1100

    Contravariance explained

--- src/22-variance-over-type-params.ts
+++ src/22-variance-over-type-params.ts
@@ -33,8 +33,39 @@ let snackProducer: Producer<Snack> = {
 
 
 snackProducer = cookieProducer  //Ok
-cookieProducer = snackProducer  //Not Ok
+//cookieProducer = snackProducer  //Not Ok
 
-// Cookie is a subtype of Snack
-// Producer(Cookie) is a subtype of Producer(Snack)
+// Cookie is a subtype of Snack     -- is a -->
+// Producer(Cookie) is a subtype of Producer(Snack)     -- is a -->
 // use keyword out to express Covariance
+
+
+// Contravariance
+
+interface Packager<T> {
+    package: (item: T) => void
+}
+
+let cookiePackager: Packager<Cookie> = {
+    package(item: Cookie) {}
+}
+
+let snackPackager: Packager<Snack> = {
+    package(item: Snack) {
+        if (item instanceof Cookie) {
+            // Package Cookie
+        }
+        else if (item instanceof Pretzel) {
+            // Package Pretzel
+        } else {
+            // Package Misc
+        }
+    }
+}
+
+//Type Equivalence Check
+cookiePackager = snackPackager  //Ok
+snackPackager = cookiePackager  //Not Ok
+
+// Cookie is a Snack       -- is a -->
+// BUT Packager<Snack> is a Packager<Cookie>    <-- is a --
